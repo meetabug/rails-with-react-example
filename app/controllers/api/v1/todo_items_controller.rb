015d8024aa3b2d790f740ff6e57732b0a57d9925
meetabug <1272526724@qq.com>
2020-04-19 23:10:42 +0800

    Update The Create Action

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -17,6 +17,18 @@ class Api::V1::TodoItemsController < ApplicationController
   end
 
   def create
+    @todo_item = current_user.todo_items.build(todo_item_params)
+    if authorized?
+      respond_to do |format|
+        if @todo_item.save
+          format.json { render :show, status: :created, location: api_v1_todo_item_path(@todo_item) }
+        else
+          format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def update
@@ -41,5 +53,9 @@ class Api::V1::TodoItemsController < ApplicationController
         format.json { render :unauthorized, status: 401 }
       end
     end
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit(:title, :complete)
   end
 end
