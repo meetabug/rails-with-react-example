66d9017c3ffdca08293e9cd4135ce72d41dd68b6
meetabug <1272526724@qq.com>
2020-04-19 21:57:31 +0800

    Lock Down The Controller and  Update The Index Action and Show Action

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @todo_items = TodoItem.all
+    @todo_items = current_user.todo_items.all
   end
 
   def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def create
@@ -21,5 +29,17 @@ class Api::V1::TodoItemsController < ApplicationController
 
   def set_todo_item
     @todo_item = TodoItem.find params[:id]
+  end
+
+  def authorized?
+    @todo_item.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end
   end
 end
