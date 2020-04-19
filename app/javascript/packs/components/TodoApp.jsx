448dc77cf8d85901abcd4a8e9a9fd1d949885b34
meetabug <1272526724@qq.com>
2020-04-19 23:43:21 +0800

    Render TodoItem and TodoItem Components

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
+import TodoItems from './TodoItems'
+import TodoItem from './TodoItem'
 class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoItems: []
+        };
+        this.getTodoItems = this.getTodoItems.bind(this);
+    }
+    componentDidMount() {
+        this.getTodoItems();
+    }
+    getTodoItems() {
+        axios
+            .get("/api/v1/todo_items")
+            .then(response => {
+                const todoItems = response.data;
+                this.setState({ todoItems });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     render() {
-        return <p>TodoApp</p>
+        return (
+            <TodoItems>
+                {this.state.todoItems.map(todoItem => (
+                    <TodoItem key={todoItem.id} todoItem={todoItem} />
+                ))}
+            </TodoItems>
+        );
     }
 }
 
