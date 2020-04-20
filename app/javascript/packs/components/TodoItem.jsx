2d20bfcb47c21d210c881a7fc818a34cfa41eb27
meetabug <1272526724@qq.com>
2020-04-20 11:59:22 +0800

    Filtering TodoItems

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -54,7 +54,9 @@ class TodoItem extends React.Component {
     render() {
         const { todoItem } = this.props
         return (
-            <tr className={`${this.state.complete ? 'table-light' : ''}`}>
+            <tr
+                className={`${ this.state.complete && this.props.hideCompletedTodoItems ? `d-none` : "" } ${this.state.complete ? "table-light" : ""}`}
+            >
                 <td>
                     <svg
                         className={`bi bi-check-circle ${
@@ -119,5 +121,6 @@ export default TodoItem
 
 TodoItem.propTypes = {
     todoItem: PropTypes.object.isRequired,
-    getTodoItems: PropTypes.func.isRequired
+    getTodoItems: PropTypes.func.isRequired,
+    hideCompletedTodoItems: PropTypes.bool.isRequired
 }
